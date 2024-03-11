00f5ca49e881358c04025487be302344cb4c884f
unknown <Sena@MEDAPRCSGFSP830.FormacionSena.RED>
2024-03-11 11:22:24 -0500

    CONSUMIR API

---
+++ consumir_api/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:consumir_api/listar_departamentos.dart';
+import 'package:consumir_api/listar_hurtos.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const Main());
+}
+
+class Main extends StatefulWidget {
+  const Main({super.key});
+
+  @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ListarHurtos(),
+    );
+  }
+}
